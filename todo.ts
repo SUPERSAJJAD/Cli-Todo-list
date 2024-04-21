6c3b49c4fa0afda3a8ad98bd87d034eafd23f4ae
Sajjad Lashari <khansahab0289@gmail.com>
2024-04-21 18:04:12 +0500

    Cli-Todo project
    "I am thrilled to share my project and would like to express my gratitude towards the teachers who helped me along the way. A big thank you to all of the Teachers!"

---
+++ todo.ts
@@ -0,0 +1,28 @@
+#!usr/bin/env node
+import inquirer from "inquirer"
+
+let todos =[]
+let condition =true;
+while(condition) 
+{
+    let todoquestions= await inquirer.prompt(
+[
+
+        {
+        name:"todolist",
+        type:"input",
+        message:"What would you like to add in todo?"
+    },
+
+    {
+        name:"question1",
+        type:"confirm",
+        message:"Would you like to add more in your todo? ",
+        default:"true"
+    }
+    ] 
+);
+todos.push (todoquestions.todolist);
+console.log(todos)
+condition = todoquestions.question1;
+}
